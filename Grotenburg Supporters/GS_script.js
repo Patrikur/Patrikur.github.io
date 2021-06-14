985f34a69312ec4cea36cb817bb0c8575fb582aa
Patrikur <82215196+Patrikur@users.noreply.github.com>
2021-06-14 18:05:38 +0000

    Add files via upload

---
+++ Grotenburg Supporters/GS_script.js
@@ -0,0 +1,104 @@
+const sponsors = ['Boels_Logo.gif','BZB_Logo.png','Dauner_Logo.jpg','Gehlings_Bausysteme_Logo.png','Hauser_Logo.png','Haushaltsauflösungen_Logo.png','Hendrichs_Logo.png','Lomme_Logo.png','Peschges_Logo.png','Stufffactory_Logo.jpg','Supporters Club Krefeld_Logo.png','TheaterKRMG_Logo.png','VoBaKrefeld_Logo.png'];
+`<img src="/Logos/${sponsors[i]}" alt="Boels Logo" />`;
+
+const data = {
+    "print": [
+      {
+        "headline": "Grotenburg: Fans helfen bei Stadion-Sanierung in Krefeld mit",
+        "link": "https://www1.wdr.de/nachrichten/grotenburg-fans-krefeld-handwerker-100.html",
+        "picture": "https://www1.wdr.de/nachrichten/rheinland/grotenburg-stadion-sanierung-zeitplan-krefeld-100~_v-gseapremiumxl.jpg",
+        "date": "44284",
+        "source": "WDR",
+        "region": "regional"
+      },
+      {
+        "headline": "KFC Uerdingen: 450 Fans helfen bei Sanierung der Grotenburg",
+        "link": "https://www.reviersport.de/artikel/kfc-uerdingen-450-fans-helfen-bei-sanierung-der-grotenburg/",
+        "picture": "https://www.reviersport.de/fotos/at/full/411347f6-dda3-4a6c-9cbc-58200e145aa6",
+        "date": "44302",
+        "source": "Reviersport",
+        "region": "regional"
+      },
+      {
+        "headline": "Grotenburg: 400 Fans des KFC Uerdingen wollen mit dem Umbau loslegen",
+        "link": "https://www.wz.de/sport/fussball/kfc-uerdingen/grotenburg-400-fans-des-kfc-uerdingen-wollen-mit-dem-umbau-loslegen_aid-57390113",
+        "picture": "https://www.wz.de/imgs/39/1/0/3/4/6/8/0/2/1/tok_b5b18d3edc2e00442ec9f5415ff9ec20/w950_h526_x646_y307_WZ_Umbau_an_der_Grotenburg009-383689f270320963.jpg",
+        "date": "44337",
+        "source": "Westdeutsche Zeitung",
+        "region": "regional"
+      },
+      {
+        "headline": "450 Fans helfen bei der Sanierung der Grotenburg",
+        "link": "https://rp-online.de/nrw/staedte/krefeld/krefeld-fans-des-kfc-uerdingen-helfen-bei-der-sanierung-der-grotenburg_aid-58320557",
+        "picture": "https://rp-online.de/imgs/32/1/0/5/7/4/3/2/6/7/tok_4af326915d609f8e30340bfd544d442e/w950_h526򹱲64_Sanierung_Grotenburg-e953a5a17f56e24a.JPG",
+        "date": "44337",
+        "source": "Rheinische Post",
+        "region": "regional"
+      },
+      {
+        "headline": "Grotenburg: KFC-Fans starten im Juni mit den Arbeiten",
+        "link": "https://www.wz.de/sport/fussball/kfc-uerdingen/grotenburg-kfc-fans-starten-im-juni-mit-den-arbeiten_aid-58324003",
+        "picture": "https://www.wz.de/imgs/39/1/0/5/7/5/0/2/1/9/tok_8294ff448083935bd3c18c2f6ea83a23/w950_h526󹱱17_WZ_Sanierung_Grotenburg_Helfer010-189299c0bb1fb641.jpg",
+        "date": "44337",
+        "source": "Westdeutsche Zeitung",
+        "region": "regional"
+      },
+      {
+        "headline": "Grotenburg: Fans packen mit an",
+        "link": "https://www.mein-krefeld.de/grotenburg-fans-packen-mit-an_aid-58318999",
+        "picture": "https://www.mein-krefeld.de/imgs/67/1/0/5/7/4/0/2/7/7/tok_8a8960a4a05fbed1e2446e8e14b809c3/w950_h526񹱱10_Grotenburg-Fans-aaf04f7c7c4a6f16.jpg",
+        "date": "44337",
+        "source": "City Anzeigenblatt Krefeld",
+        "region": "regional"
+      },
+      {
+        "headline": "450 Fans helfen bei Sanierung der Krefelder Grotenburg",
+        "link": "https://www.zeit.de/news/2021-05/21/450-fans-helfen-bei-sanierung-der-krefelder-grotenburg",
+        "picture": "null",
+        "date": "44337",
+        "source": "Zeit Online",
+        "region": "national"
+      },
+      {
+        "headline": "450 Fans helfen bei Sanierung der Krefelder Grotenburg",
+        "link": "https://www.sueddeutsche.de/sport/fussball-krefeld-450-fans-helfen-bei-sanierung-der-krefelder-grotenburg-dpa.urn-newsml-dpa-com-20090101-210521-99-692612",
+        "picture": "null",
+        "date": "44337",
+        "source": "Süddeutsche Zeitung Online",
+        "region": "national"
+      },
+      {
+        "headline": "450 Fans helfen bei Sanierung der Krefelder Grotenburg",
+        "link": "https://www.rtl.de/cms/450-fans-helfen-bei-sanierung-der-krefelder-grotenburg-4764557.html",
+        "picture": "null",
+        "date": "44337",
+        "source": "RTL.de",
+        "region": "national"
+      },
+      {
+        "headline": "450 Fans helfen bei Sanierung der Krefelder Grotenburg",
+        "link": "https://www.msn.com/de-de/sport/fussball/450-fans-helfen-bei-sanierung-der-krefelder-grotenburg/ar-AAKeHUD",
+        "picture": "null",
+        "date": "44337",
+        "source": "MSN",
+        "region": "national"
+      },
+      {
+        "headline": "Wunder von der Grotenburg",
+        "link": "https://www.mein-krefeld.de/die-stadt/das-wunder-von-der-grotenburg-als-theaterstueck_aid-59232867",
+        "picture": "https://www.mein-krefeld.de/imgs/67/1/0/7/3/8/6/0/5/3/tok_be7451ff0f48933ddfe6d13f2b170b95/w950_h526򹱲68_Das_Wunder_der_Grotenburg-fc18af1799d8d315.jpg",
+        "date": "44357",
+        "source": "City Anzeigenblatt Krefeld",
+        "region": "regional"
+      }
+    ],
+    "video": [
+      {
+        "headline": "450 Fans helfen bei Sanierung der Krefelder Grotenburg",
+        "link": "https://www.rtl.de/cms/450-fans-helfen-bei-sanierung-der-krefelder-grotenburg-4764557.html",
+        "picture": "null",
+        "date": "44337",
+        "source": "RTL.de"
+      }
+    ]
+  }
