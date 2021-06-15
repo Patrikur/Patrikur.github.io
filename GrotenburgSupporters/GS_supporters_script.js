2f498a4fcf3fdee326939b707f5f57637e91d97c
Patrikur <82215196+Patrikur@users.noreply.github.com>
2021-06-15 14:23:00 +0000

    Add files via upload

---
+++ GrotenburgSupporters/GS_supporters_script.js
@@ -0,0 +1,11 @@
+const sponsors = ['Boels_Logo.gif','BZB_Logo.png','Dauner_Logo.jpg','Gehlings_Bausysteme_Logo.png','Hauser_Logo.png','Haushaltsauflösungen_Logo.png','Hendrichs_Logo.png','KFC_Logo.png','Lomme_Logo.png','Peschges_Logo.png','Stufffactory_Logo.jpg','Supporters Club Krefeld_Logo.png','TheaterKRMG_Logo.png','VoBaKrefeld_Logo.png'];
+const main = document.querySelector('.main-container');
+
+
+sponsors.forEach(sponsor => {
+    main.innerHTML += `
+    <div class="supporter">
+      <img src="/Logos/${sponsor}" alt="" />
+    </div>
+    `
+})
