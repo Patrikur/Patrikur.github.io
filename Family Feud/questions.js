01cd89a96d2c1cdbe95e6fefdc210f960c671680
Patrikur <82215196+Patrikur@users.noreply.github.com>
2021-08-06 15:27:42 +0000

    Implemented Code

---
+++ Family Feud/questions.js
@@ -0,0 +1,88 @@
+const questions = [
+    {
+        question: 'Most WWE Championship Title Reigns',
+        1: {
+            answer:'John Cena',
+            points: 25
+        },
+        2:{
+            answer:'Randy Orton',
+            points: 19
+        },
+        3:{
+            answer:'Triple H',
+            points: 17
+        },
+        4:{
+            answer:'The Rock',
+            points:15
+        },
+        5:{
+            answer:'Hulk Hogan',
+            points:12
+        }, 
+        6:{
+            answer:'Steve Austin',
+            points:12
+        }
+    },
+    {
+        question: 'Most televised Shows not produced by WWE',
+        1:{
+            answer:'Impact Xplosion',
+            points: 58
+        },
+        2:{
+            answer:'Impact',
+            points: 54
+        },
+        3:{
+            answer:'Championship Wrestling From Hollywood',
+            points: 31
+        },
+        4:{
+            answer:'Ring of Honor TV',
+            points:30
+        },
+        5:{
+            answer: 'ECW Hardcore TV',
+            points:27
+        }
+    },
+    {
+        question:'Biggest Wrestling Events Except WrestleMania',
+        1:{
+            answer:'Collision in Korea',
+            points:178
+        },
+        2:{
+            answer:'SummerSlam 92',
+            points:45
+        },
+        3:{
+            answer:'Super Show-Down',
+            points:39
+        },
+        4:{
+            answer:'Antonio Inoki Retirement',
+            points:39
+        }
+    },
+    {
+        question:'Highest Rated Wrestling Games on Metacritic',
+        1:{
+            answer:'WWF SmackDown! 2',
+            points: 136
+        },
+        2:{
+            answer:'WWF No Mercy',
+            points: 135
+        },
+        3:{
+            answer:'WWE SmackDown! Here come´s the pain',
+            points: 129
+        }
+    }    
+]
+
+export { questions };
--- Family Feud/script.js
+++ Family Feud/script.js
@@ -1,93 +1,135 @@
-const questions = [
-    {
-        question: 'Most WWE Championship Title Reigns',
-        1: {
-            answer:'John Cena',
-            points: 25
-        },
-        2:{
-            answer:'Randy Orton',
-            points: 19
-        },
-        3:{
-            answer:'Triple H',
-            points: 17
-        },
-        4:{
-            answer:'The Rock',
-            points:15
-        },
-        5:{
-            answer:'Hulk Hogan',
-            points:12
-        }, 
-        6:{
-            answer:'Steve Austin',
-            points:12
-        }
-    },
-    {
-        question: 'Most televised Shows not produced by WWE',
-        1:{
-            answer:'Impact Xplosion',
-            points: 58
-        },
-        2:{
-            answer:'Impact',
-            points: 54
-        },
-        3:{
-            answer:'Championship Wrestling From Hollywood',
-            points: 31
-        },
-        4:{
-            answer:'Ring of Honor TV',
-            points:30
-        },
-        5:{
-            answer: 'ECW Hardcore TV',
-            points:27
-        }
-    },
-    {
-        question:'Biggest Wrestling Events Except WrestleMania',
-        1:{
-            answer:'Collision in Korea',
-            points:178
-        },
-        2:{
-            answer:'SummerSlam 92',
-            points:45
-        },
-        3:{
-            answer:'Super Show-Down',
-            points:39
-        },
-        4:{
-            answer:'Antonio Inoki Retirement',
-            points:39
-        }
-    },
-    {
-        question:'Highest Rated Wrestling Games on Metacritic',
-        1:{
-            answer:'WWF SmackDown! 2',
-            points: 136
-        },
-        2:{
-            answer:'WWF No Mercy',
-            points: 135
-        },
-        3:{
-            answer:'WWE SmackDown! Here come´s the pain',
-            points: 129
-        }
-    }    
-]
+import { questions } from "./questions.js";
 
-const answers = document.querySelector('.answers');
-const points = document.querySelector('.points');
+const question = document.getElementById('question');
+const answers = document.getElementById('answers');
+const points = document.getElementById('points');
+const crossTeamL = document.getElementById('cross-teamL');
+const crossTeamR = document.getElementById('cross-teamR');
+const teamLName = document.getElementById('teamL-name');
+const teamRName = document.getElementById('teamR-name');
+const teamLPoints = document.getElementById('teamL-points');
+const teamRPoints = document.getElementById('teamR-points');
+const leftArrow = document.getElementById('leftArrow');
+const rightArrow = document.getElementById('rightArrow');
+const currentRound = document.getElementById('currentRound');
+
+//Basic settings
+let teamL;
+let teamR;
+
+let round = 0;
+
+let turn;
 
 let scoreL = 0;
 let scoreR = 0;
 let scoreRound = 0;
+
+question.innerHTML = `${questions[round].question}`;
+
+function loadRound (round){
+  for(let i=1; i< Object.keys(questions[round]).length; i++){
+    answers.innerHTML += `
+    <p>${i}. <span id="answer${i}"></span></p>
+    `
+    points.innerHTML += `
+    <p id="points${i}">0</p>
+    `
+  }
+  currentRound.innerText = scoreRound;
+  teamLPoints.innerText = scoreL;
+  teamRPoints.innerText = scoreR;  
+}
+
+loadRound(round);
+
+//Choose team
+window.addEventListener('keyup', (e) =>{
+    switch(e.key){
+        case 'ArrowLeft':
+            leftArrow.classList.add('active');
+            rightArrow.classList.remove('active');
+            turn = "left";
+            break;
+        case 'ArrowRight':
+            rightArrow.classList.add('active');
+            leftArrow.classList.remove('active');
+            turn = "right";
+    }
+})
+
+//Get answers and points
+const answer1 = document.getElementById('answer1');
+const answer2 = document.getElementById('answer2');
+const answer3 = document.getElementById('answer3');
+const answer4 = document.getElementById('answer4');
+const answer5 = document.getElementById('answer5');
+const answer6 = document.getElementById('answer6');
+const points1 = document.getElementById('points1');
+const points2 = document.getElementById('points2');
+const points3 = document.getElementById('points3');
+const points4 = document.getElementById('points4');
+const points5 = document.getElementById('points5');
+const points6 = document.getElementById('points6');
+
+window.addEventListener('keyup', (e) =>{
+    switch(e.key){
+    case '1':
+        answer1.innerHTML = `${questions[round][1].answer}`;
+        points1.innerHTML = `${questions[round][1].points}`;
+        scoreRound += questions[round][1].points;
+        currentRound.innerText = scoreRound;
+        break;
+    case '2':
+        answer2.innerHTML = `${questions[round][2].answer}`;
+        points2.innerHTML = `${questions[round][2].points}`;
+        scoreRound += questions[round][2].points;
+        currentRound.innerText = scoreRound;
+        break;
+    case '3':
+        answer3.innerHTML = `${questions[round][3].answer}`;
+        points3.innerHTML = `${questions[round][3].points}`;
+        scoreRound += questions[round][3].points;
+        currentRound.innerText = scoreRound;
+        break;
+    case '4':
+        answer4.innerHTML = `${questions[round][4].answer}`;
+        points4.innerHTML = `${questions[round][4].points}`;
+        scoreRound += questions[round][4].points;
+        currentRound.innerText = scoreRound;
+        break;        
+    case '5':
+        answer5.innerHTML = `${questions[round][5].answer}`;
+        points5.innerHTML = `${questions[round][5].points}`;
+        scoreRound += questions[round][5].points;
+        currentRound.innerText = scoreRound;
+        break;
+    case '6':
+        answer6.innerHTML = `${questions[round][6].answer}`;
+        points6.innerHTML = `${questions[round][6].points}`;
+        scoreRound += questions[round][6].points;
+        currentRound.innerText = scoreRound;
+        break;
+    case 'x':
+        if(turn === "left" && crossTeamL.innerText.length <= 2){
+            crossTeamL.innerText += 'X';
+            if(crossTeamL.innerText.length === 3){
+                rightArrow.classList.add('active');
+                leftArrow.classList.remove('active');
+                turn = "right";
+            }
+        } else if (turn === "right" && crossTeamR.innerText.length <= 2) {
+            crossTeamR.innerText += 'X';
+            if(crossTeamR.innerText.length === 3){
+                leftArrow.classList.add('active');
+                rightArrow.classList.remove('active');
+                turn = "left";
+            }
+        } else if(turn === "left" && crossTeamR.innerText.length === 3){
+            crossTeamL.innerText += 'XXX';
+        } else if(turn === "right" && crossTeamL.innerText.length === 3){
+            crossTeamL.innerText += 'XXX';
+        }
+    }
+})
+
