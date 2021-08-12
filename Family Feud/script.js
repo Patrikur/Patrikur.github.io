1de0c7f2acc9503b938aaf07af983fa564e9a9c9
Patrikur <82215196+Patrikur@users.noreply.github.com>
2021-08-12 12:28:51 +0000

    further improvements of the code

--- Family Feud/script.js
+++ Family Feud/script.js
@@ -18,6 +18,7 @@ let teamL;
 let teamR;
 
 let round = 0;
+let playingRound = true;
 
 let turn;
 
@@ -25,38 +26,57 @@ let scoreL = 0;
 let scoreR = 0;
 let scoreRound = 0;
 
-question.innerHTML = `${questions[round].question}`;
+//Choose random team
+function randomTeam(){
+    const rng = Math.round(Math.random()*100);
+    if(rng % 2 === 0){
+        leftArrow.innerHTML = `&#9679;`;
+        rightArrow.innerHTML = `&#9676;`;
+        turn = "left";  
+    } else {
+        rightArrow.innerHTML = `&#9679;`;
+        leftArrow.innerHTML = `&#9676;`;
+        turn = "right";
+    }
+}
 
+//load current round
 function loadRound (round){
-  for(let i=1; i< Object.keys(questions[round]).length; i++){
-    answers.innerHTML += `
-    <p>${i}. <span id="answer${i}"></span></p>
-    `
-    points.innerHTML += `
-    <p id="points${i}">0</p>
-    `
-  }
-  currentRound.innerText = scoreRound;
-  teamLPoints.innerText = scoreL;
-  teamRPoints.innerText = scoreR;  
+    answers.innerHTML = "";
+    points.innerHTML = "";
+    crossTeamR.innerText = '';
+    crossTeamL.innerText = '';
+
+    for(let i=1; i< Object.keys(questions[round]).length; i++){
+        answers.innerHTML += `
+        <p>${i}. <span id="answer${i}"></span></p>
+        `
+        points.innerHTML += `
+        <p id="points${i}">0</p>
+        `
+    }
+    question.innerHTML = `${questions[round].question}`;
+    currentRound.innerText = 0;
+    teamLPoints.innerText = scoreL;
+    teamRPoints.innerText = scoreR;
+    playingRound = true;
+    randomTeam();
 }
 
 loadRound(round);
 
-//Choose team
-window.addEventListener('keyup', (e) =>{
-    switch(e.key){
-        case 'ArrowLeft':
-            leftArrow.classList.add('active');
-            rightArrow.classList.remove('active');
-            turn = "left";
-            break;
-        case 'ArrowRight':
-            rightArrow.classList.add('active');
-            leftArrow.classList.remove('active');
-            turn = "right";
+//Finish round and add points to total
+function finishRound(){
+    if(turn === "left"){
+        scoreL += scoreRound;
+        teamLPoints.innerText = scoreL;
+        playingRound = false; 
+    } else if (turn === "right"){
+        scoreR += scoreRound;
+        teamRPoints.innerText = scoreR;
+        playingRound = false;
     }
-})
+}
 
 //Get answers and points
 const answer1 = document.getElementById('answer1');
@@ -75,57 +95,120 @@ const points6 = document.getElementById('points6');
 window.addEventListener('keyup', (e) =>{
     switch(e.key){
     case '1':
-        answer1.innerHTML = `${questions[round][1].answer}`;
-        points1.innerHTML = `${questions[round][1].points}`;
-        scoreRound += questions[round][1].points;
-        currentRound.innerText = scoreRound;
+        if(points1.innerHTML != `${questions[round][1].points}` && (crossTeamL.innerText.length != 3 || crossTeamR.innerText.length != 3)){
+            answer1.innerHTML = `${questions[round][1].answer}`;
+            points1.innerHTML = `${questions[round][1].points}`;
+            scoreRound += questions[round][1].points;
+            currentRound.innerText = scoreRound;
+            if(Number.isInteger(scoreRound/100)){
+                finishRound();
+            }   
+        } else {
+            answer1.innerHTML = `${questions[round][1].answer}`;
+            points1.innerHTML = `${questions[round][1].points}`;
+        }
         break;
     case '2':
-        answer2.innerHTML = `${questions[round][2].answer}`;
-        points2.innerHTML = `${questions[round][2].points}`;
-        scoreRound += questions[round][2].points;
-        currentRound.innerText = scoreRound;
+        if(points2.innerHTML != `${questions[round][2].points}` && (crossTeamL.innerText.length != 3 || crossTeamR.innerText.length != 3)){
+            answer2.innerHTML = `${questions[round][2].answer}`;
+            points2.innerHTML = `${questions[round][2].points}`;
+            scoreRound += questions[round][2].points;
+            currentRound.innerText = scoreRound;
+            if(Number.isInteger(scoreRound/100)){
+                finishRound();
+            }   
+        } else {
+            answer2.innerHTML = `${questions[round][2].answer}`;
+            points2.innerHTML = `${questions[round][2].points}`;
+        }
         break;
     case '3':
-        answer3.innerHTML = `${questions[round][3].answer}`;
-        points3.innerHTML = `${questions[round][3].points}`;
-        scoreRound += questions[round][3].points;
-        currentRound.innerText = scoreRound;
+        if(points3.innerHTML != `${questions[round][3].points}` && (crossTeamL.innerText.length != 3 || crossTeamR.innerText.length != 3)){
+            answer3.innerHTML = `${questions[round][3].answer}`;
+            points3.innerHTML = `${questions[round][3].points}`;
+            scoreRound += questions[round][3].points;
+            currentRound.innerText = scoreRound;
+            if(Number.isInteger(scoreRound/100)){
+                finishRound();
+            }   
+        } else {
+            answer3.innerHTML = `${questions[round][3].answer}`;
+            points3.innerHTML = `${questions[round][3].points}`;
+        }
         break;
     case '4':
-        answer4.innerHTML = `${questions[round][4].answer}`;
-        points4.innerHTML = `${questions[round][4].points}`;
-        scoreRound += questions[round][4].points;
-        currentRound.innerText = scoreRound;
+        if(points4.innerHTML != `${questions[round][4].points}` && (crossTeamL.innerText.length != 3 || crossTeamR.innerText.length != 3)){
+            answer4.innerHTML = `${questions[round][4].answer}`;
+            points4.innerHTML = `${questions[round][4].points}`;
+            scoreRound += questions[round][4].points;
+            currentRound.innerText = scoreRound;
+            if(Number.isInteger(scoreRound/100)){
+                finishRound();
+            }   
+        } else {
+            answer4.innerHTML = `${questions[round][4].answer}`;
+            points4.innerHTML = `${questions[round][4].points}`;
+        }
         break;        
     case '5':
-        answer5.innerHTML = `${questions[round][5].answer}`;
-        points5.innerHTML = `${questions[round][5].points}`;
-        scoreRound += questions[round][5].points;
-        currentRound.innerText = scoreRound;
+        if(points5.innerHTML != `${questions[round][5].points}` && (crossTeamL.innerText.length != 3 || crossTeamR.innerText.length != 3)){
+            answer5.innerHTML = `${questions[round][5].answer}`;
+            points5.innerHTML = `${questions[round][5].points}`;
+            scoreRound += questions[round][5].points;
+            currentRound.innerText = scoreRound;
+            if(Number.isInteger(scoreRound/100)){
+                finishRound();
+            }   
+        } else {
+            answer5.innerHTML = `${questions[round][5].answer}`;
+            points5.innerHTML = `${questions[round][5].points}`;
+        }
         break;
     case '6':
-        answer6.innerHTML = `${questions[round][6].answer}`;
-        points6.innerHTML = `${questions[round][6].points}`;
-        scoreRound += questions[round][6].points;
-        currentRound.innerText = scoreRound;
+        if(points6.innerHTML != `${questions[round][6].points}` && (crossTeamL.innerText.length != 3 || crossTeamR.innerText.length != 3)){
+            answer6.innerHTML = `${questions[round][6].answer}`;
+            points6.innerHTML = `${questions[round][6].points}`;
+            scoreRound += questions[round][6].points;
+            currentRound.innerText = scoreRound;
+            if(Number.isInteger(scoreRound/100)){
+                finishRound();
+            }   
+        } else {
+            answer6.innerHTML = `${questions[round][6].answer}`;1
+            points6.innerHTML = `${questions[round][6].points}`;
+        }
         break;
     case 'x':
-        if(turn === "left" && crossTeamL.innerText.length <= 2){
+        console.log(e.key);
+        if (turn === "right" && crossTeamL.innerText.length === 3) {
+            crossTeamR.innerText = 'XXX';
+            turn = "left";
+            finishRound();
+        } else if (turn === "left" && crossTeamR.innerText.length === 3) {
+            crossTeamL.innerText = 'XXX';
+            turn = "right";
+            finishRound();
+        } else if (turn === "left" && crossTeamL.innerText.length < 3){
             crossTeamL.innerText += 'X';
             if(crossTeamL.innerText.length === 3){
-                rightArrow.classList.add('active');
-                leftArrow.classList.remove('active');
+                rightArrow.innerHTML = `&#9679;`;
+                leftArrow.innerHTML = `&#9676;`;
                 turn = "right";
             }
-        } else if (turn === "right" && crossTeamR.innerText.length <= 2) {
+        } else if (turn === "right" && crossTeamR.innerText.length < 3) {
             crossTeamR.innerText += 'X';
             if(crossTeamR.innerText.length === 3){
-                leftArrow.classList.add('active');
-                rightArrow.classList.remove('active');
+                leftArrow.innerHTML = `&#9679;`;
+                rightArrow.innerHTML = `&#9676;`;
                 turn = "left";
             }
         }
+    case 'Enter':
+        if(round < questions.length-1){
+            round += 1;
+            loadRound (round);
+        }
+        
     }
 })
 
